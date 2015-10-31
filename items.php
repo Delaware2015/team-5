f77c5229cd897f0392eb8cb005f9359aa3681e2e
Eric Rosenberg <EricR>
2015-10-31 00:28:30 -0400

    More items edits

--- items.php
+++ items.php
@@ -9,9 +9,8 @@
   <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <div class="container">
-  <h2>GoodWill Stores Near You</h2>            
+  <h2>GoodWill Stores in the Area</h2>            
   <table class="table table-striped">
     <thead>
       <tr>
@@ -23,17 +22,14 @@
       <tr>
         <td>John</td>
         <td>Doe</td>
-        <td>john@example.com</td>
       </tr>
       <tr>
         <td>Mary</td>
         <td>Moe</td>
-        <td>mary@example.com</td>
       </tr>
       <tr>
         <td>July</td>
         <td>Dooley</td>
-        <td>july@example.com</td>
       </tr>
     </tbody>
   </table>
