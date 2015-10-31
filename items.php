55729769520ab3df728d3f50e0ad26b692f170c4
Eric Rosenberg <EricR>
2015-10-31 00:03:34 -0400

    Edit items

--- items.php
+++ items.php
@@ -1,8 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
 <body>
-<h1>Stores Near You</h1>
+
+<div class="container">
+  <h2>GoodWill Stores Near You</h2>            
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Location</th>
+        <th>Items Requested</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
 </body>
-</head>
 </html>
