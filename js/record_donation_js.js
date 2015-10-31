4011510e52c33135ca57d177b6e92516e01125c7
D-Sean <sdillon255@gmail.com>
2015-10-31 04:36:02 -0400

    Dynamic donation form

---
+++ js/record_donation_js.js
@@ -0,0 +1,26 @@
+//create an html element
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+//create new donation field
+var numOfDonations = 1;
+function addNewDonationField(){
+	//alert("hello");
+	numOfDonations = numOfDonations + 1;
+	document.getElementById("num_of_donations").value = numOfDonations;
+	var donation_div = document.getElementById("donation_div");
+	//var fragment = create('<center><hr><div class = "form-inline">	<label for"job' + numOfJobs + '_text">Job ' + numOfJobs + ' </label>	<input type="text" class="form-control" id="job' + numOfJobs + '_text" name="job' + numOfJobs + '_text" placeholder="Job description" maxlength="75">	<input type="date" class="form-control" id="job' + numOfJobs + '_start_date" name="job' + numOfJobs + '_start_date"><input type="date" class="form-control" id="job' + numOfJobs + '_end_date" name="job' + numOfJobs + '_end_date"></div></center>');
+	//var fragment = create('<center><hr><div class = "form-inline form-group has-feedback" id ="job-div"><label for"job' + numOfJobs + '_text">Job ' + numOfJobs + '</label><input type="text" class="form-control" id="job' + numOfJobs + '_text" name="job' + numOfJobs + '_text" placeholder="Job description" maxlength="150" required><input type="date" class="form-control" id="job' + numOfJobs + '_start_date" name="job' + numOfJobs + '_start_date" required><input type="date" class="form-control" id="job' + numOfJobs + '_end_date" name="job' + numOfJobs + '_end_date" required></div></center>');	
+	
+	var fragment = create('<hr><div class="form-group"><label for="donation_type' + numOfDonations + '">Donation Type</label><select class="form-control" id="donation_type' + numOfDonations + '" name="donation_type' + numOfDonations + '" required><option value="" selected disabled hidden>Donation Category</option><option value="1">Clothing</option><option value="2">Appliances/Stereos</option><option value="3">Cars/Trucks/RVs/ Boats/Motorcycles</option><option value="4">Dictionaries/Text Books/Encyclopedias</option><option value="5">Furniture</option><option value="6">Household Items</option><option value="7">Furnishings</option><option value="8">Computers and all Peripherals</option><option value="9">Records/CDs/Tapes/DVDs/Video Games</option><option value="10">Sporting Goods and Exercise Equipment</option></select>	</div><div class="form-group"><label for="amount1">Amount of Donation Items</label><input type="number" class="form-control" id="amount' + numOfDonations + '" name="amount' + numOfDonations + '" min="1" placeholder = "Amount of Donated Items" required></div>');
+	
+	donation_div.appendChild(fragment);
+	
+}
--- record_donation.php
+++ record_donation.php
@@ -10,18 +10,44 @@
 	
 	
 	if (isset($_POST["save"])){
-	
-		$fname = $_POST['fname'];
-		$lname = $_POST['lname'];
-		$email = $_POST['email'];
-		$pwd = $_POST['pwd'];
-		$pwd_confirm = $_POST['pwd_confirm'];
-		$zipcode = $_POST['zipcode'];
+		$user_id = $_SESSION['user_id'];
+		$donation_type1 = $_POST['donation_type1'];
+		$amount1 = $_POST['amount1'];
+		$store_id = '12345';
+		$timestamp = date("Y-n-d G:i:s");
 		
-		//make session vars
-		$_SESSION['email'] = $email;
-		$_SESSION['zipcode'] = $zipcode;
+		//insert into User_Donation
+		$donationQuery = "INSERT INTO User_Donation (user_id, store_id, timestamp) VALUES ('$user_id', '$store_id', '$timestamp')";
+		if($r = mysql_query($donationQuery)){
+			echo "cheah brah";
+		}else{
+			echo"dog";
+		}
 		
+		//now get the don_id and insert into Donation_info
+		$donationInfoQuery = "";
+		
+		
+		
+		if(isset($_POST['num-of-donations'])){				
+			for ($x = 1; $x <= $_POST['num-of-donations']; $x++) {
+				$_text = $_POST['job'. $x .'_text'];
+				$job_start_date = $_POST['job'. $x .'_start_date'];
+				$job_end_date = $_POST['job'. $x .'_end_date'];
+				$jobQuery = "INSERT INTO JOBS (PATIENT_ID, JOB_DESCRIPTION, START_DATE, END_DATE) VALUES('$PATIENT_ID' , '$job_text', '$job_start_date', '$job_end_date')";
+					
+				if(mysql_query ($jobQuery)){
+					echo"<br>query success jobQuery $x";
+				} else {
+					echo "<br>query failure $x";
+				}
+			} 
+		}
+		
+		
+
+
+		/*
 		$registerQuery = "INSERT INTO Users (email, zip, password, first_name, last_name) VALUES ('$email', '$zipcode', '$pwd', '$fname', '$lname')";
 		if($r = mysql_query($registerQuery)){
 			print_r($_SESSION);
@@ -34,12 +60,13 @@
 		}else{
 			echo"dog";
 		}
+		*/
 		
 		
 		
-		
+		/*
 		if(isset($_POST['num-of-donations'])){				
-				for ($x = 1; $x <= $_POST['num-of-jobs']; $x++) {
+				for ($x = 1; $x <= $_POST['num-of-donations']; $x++) {
 					$job_text = $_POST['job'. $x .'_text'];
 					$job_start_date = $_POST['job'. $x .'_start_date'];
 					$job_end_date = $_POST['job'. $x .'_end_date'];
@@ -52,7 +79,7 @@
 					}
 				} 
 			}
-		
+		*/
 	
 	}
 	
@@ -95,7 +122,7 @@
 	<div class="row">
 
 		<div class="col-xs-1">
-			.col-xs-2
+			.col-xs-1
 		</div>
 		
 		<div class="col-xs-10">
@@ -105,37 +132,54 @@
 					<div class="col-xs-8"><h2><center>Record your donation</center></h2></div>
 					<div class="col-xs-2">.col-xs-2</div>
 				</div>
-				<form action="register_reg.php" method="post">
-					<div class="form-group">
-						<label for="donation_type1">Donation Type</label>
-						<select class="form-control" id="donation_type1" name="donation_type1" required>
-							<option value="" selected disabled hidden>Donation Category</option>
-							<option value="1">Clothing</option>
-							<option value="2">Appliances/Stereos</option>
-							<option value="3">Cars/Trucks/RVs/ Boats/Motorcycles</option>
-							<option value="4">Dictionaries/Text Books/Encyclopedias</option>
-							<option value="5">Furniture</option>
-							<option value="6">Household Items</option>
-							<option value="7">Furnishings</option>
-							<option value="8">Computers and all Peripherals</option>
-							<option value="9">Records/CDs/Tapes/DVDs/Video Games</option>
-							<option value="10">Sporting Goods and Exercise Equipment</option>
-						</select>	
-					</div>
-					<div class="form-group">
-						<label for="amount1">Amount of Donation Items</label>
-						<input type="number" class="form-control" id="amount1" name="amount1" min="1" placeholder = "Amount of Donated Items" required>
-					</div>
+				<form action="record_donation.php" method="post">
+					<div id="donation_div">
+						<input type="hidden" class="form-control" id="num_of_donations" name="num_of_donations" value="1">
+						<div class="form-group">
+							<label for="donation_type1">Donation Type</label>
+							<select class="form-control" id="donation_type1" name="donation_type1" required>
+								<option value="" selected disabled hidden>Donation Category</option>
+								<option value="1">Clothing</option>
+								<option value="2">Appliances/Stereos</option>
+								<option value="3">Cars/Trucks/RVs/ Boats/Motorcycles</option>
+								<option value="4">Dictionaries/Text Books/Encyclopedias</option>
+								<option value="5">Furniture</option>
+								<option value="6">Household Items</option>
+								<option value="7">Furnishings</option>
+								<option value="8">Computers and all Peripherals</option>
+								<option value="9">Records/CDs/Tapes/DVDs/Video Games</option>
+								<option value="10">Sporting Goods and Exercise Equipment</option>
+							</select>	
+						</div>
+						<div class="form-group">
+							<label for="amount1">Amount of Donation Items</label>
+							<input type="number" class="form-control" id="amount1" name="amount1" min="1" placeholder = "Amount of Donated Items" required>
+						</div>
+						
+						<div class="row"> <!-- this is a row template -->
+							<div class="col-xs-1">
+								<a class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Add Another Job" onclick="addNewDonationField()"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							</div>
+							<div class="col-xs-10">
+								<center>
+									<button type="submit" name="save" class="btn btn-primary">Save Donations</button>
+								</center>
+							</div>
+							<div class="col-xs-1">
+								.col-xs-1
+							</div>
+						</div>
 					
-					<center>
-						<button type="submit" name="save" class="btn btn-primary">Save Donations</button>
-					</center>
+					
+					
+					
+					</div>
 				</form>
 			</div>
 		</div>
 		
 		<div class="col-xs-1">
-			.col-xs-2
+			.col-xs-1
 		</div>
 		
 	</div>
@@ -153,5 +197,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
+<script src="js/record_donation_js.js"></script>
 </body>
 </html>
