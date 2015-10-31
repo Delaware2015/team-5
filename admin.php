b7573b04abc0ba55f228c9dfdc85a76918ff12ee
Eric Rosenberg <EricR>
2015-10-31 00:37:01 -0400

    New admin.php

---
+++ admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Admin Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<h1>Compose Email:</h1>
+ <form role="form">
+ <div class="form-group">
+  <textarea class="form-control" rows="5" id="message"></textarea>
+</div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+</body>
+</html>
