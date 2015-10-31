f57a0ca08541ad6b38ef87ed4d3ff3c0b2adc632
D-Sean <sdillon255@gmail.com>
2015-10-30 23:15:23 -0400

    php work

---
+++ register_reg.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+  <title>Register</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  
+</head>
+<body>
+
+<div class="container">
+
+<div class="row">
+  <div class="col-xs-2">.col-sm-4</div>
+  <div class="col-xs-8">.col-sm-4</div>
+  <div class="col-xs-2">.col-sm-4</div>
+</div>
+
+
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
